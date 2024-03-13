2099abbdbc4a9a51303038ea93bf8cf8c6d58b87
dodoyoba <162820777+dodoyoba@users.noreply.github.com>
2024-03-13 19:54:14 +0300

    version 2
    +bounces off rackets
    +bounces off the ceiling and floor

--- Ping_pong.py
+++ Ping_pong.py
@@ -3,7 +3,7 @@ from random import randint
 from time import time as tm
 # from time import *
 window = display.set_mode((700,500))
-display.set_caption('Пинг-понг')
+display.set_caption('Ping-Pong')
 background = transform.scale(image.load('kort.jpg'),(700,500))
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, w, h, player_speed):
@@ -31,14 +31,17 @@ class Player(GameSprite):
         if keys_pressed[K_s] and self.rect.y < 450:
             self.rect.y += self.speed
 
-p1 = Player('raketa.png',600,30,50,50,5)
+p1 = Player('raketa.png',620,30,50,50,5)
 p2 = Player('raketa.png',30,200,50,50,5)
+ball = GameSprite('green_ball.png',200,200,50,50,1)
 font.init()
 font2 = font.SysFont('Arial', 36)
 
 finish = False
 game = True
 clock = time.Clock()
+speed_x = 1
+speed_y = 1
 
 while game:
     for e in event.get():
@@ -48,7 +51,16 @@ while game:
         window.blit(background,(0,0))
         p1.updateL()
         p2.updateR()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        #ball.rect.x < 0
+        if sprite.collide_rect(p1, ball) or sprite.collide_rect(p2, ball):
+            speed_x *= -1
+            speed_y *= -1
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y *= -1
         p1.reset()
         p2.reset()
+        ball.reset()
     display.update()
     clock.tick(165)
